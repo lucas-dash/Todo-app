91aad2db067e4624456f0b4143b51765ec8fdad2
lucas-dash <homolka.lukas10@gmail.com>
2023-01-01 20:38:13 +0100

    edit creating task

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const form = document.querySelector(".create-task");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let color = document.querySelector("#priority");
+  console.log(color.value);
+});
