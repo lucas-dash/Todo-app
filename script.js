61e34ba8f77ccab93e33a7202974d034848c4655
lucas-dash <homolka.lukas10@gmail.com>
2023-01-02 20:45:37 +0100

    hamburger menu

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ form.addEventListener("submit", (e) => {
   let color = document.querySelector("#priority");
   console.log(color.value);
 });
+
+const nav = document.querySelector("#nav");
+const mainContent = document.querySelector(".tabule");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", (e) => {
+  // e.preventDefault();
+  hamburger.classList.toggle("is-active");
+  nav.classList.toggle("active");
+  mainContent.classList.toggle("active");
+});
