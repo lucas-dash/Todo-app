a93ebc292c68c701737069d2bf5aaec0f9953512
lucas-dash <homolka.lukas10@gmail.com>
2023-03-11 20:53:14 +0100

    edit dashboard and add quote api

--- dashboard.js
+++ dashboard.js
@@ -33,3 +33,73 @@ openSide.addEventListener('click', (e) => {
     }
   });
 });
+
+// ! task info to display
+
+// ? json data
+const todo = JSON.parse(localStorage.getItem('todo')) || [];
+const inProgress = JSON.parse(localStorage.getItem('progress')) || [];
+const completeTask = JSON.parse(localStorage.getItem('complete')) || [];
+
+// ? sidebar count task
+const sideBarCount = document.getElementById('count_task_side');
+sideBarCount.textContent = todo.length;
+
+// const allTask = [...todo, ...inProgress, ...completeTask];
+
+const tasksProgress = document.querySelector('#task-progress');
+tasksProgress.textContent = inProgress.length;
+
+// ? taks in today
+const taskToday_el = document.querySelector('#today-task');
+taskToday_el.textContent = 0;
+let todayTask = [];
+
+const date = new Date();
+const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+const formattedDate = new Intl.DateTimeFormat('en-GB', options).format(date);
+
+todo.forEach((task) => {
+  if (task._date === formattedDate) {
+    todayTask.push(task);
+  }
+});
+taskToday_el.textContent = todayTask.length;
+
+//? date now
+const dateNow = document.querySelector('#date-now');
+dateNow.textContent = formattedDate;
+
+// ? time now and welcome text
+const welcomeText = document.querySelector('#welcome-text');
+
+const timeNow = new Date().getHours();
+
+if (timeNow <= 11 && timeNow >= 6) {
+  welcomeText.textContent = 'Good Morning!';
+} else if (timeNow > 11 && timeNow <= 18) {
+  welcomeText.textContent = 'Good Afternoon!';
+} else if (timeNow > 18 && timeNow <= 24) {
+  welcomeText.textContent = 'Good Evening!';
+} else if (timeNow >= 0 && timeNow <= 5) {
+  welcomeText.textContent = 'Good night!';
+} else {
+  welcomeText.textContent = 'Have a Nice Day!';
+}
+
+// ? motivation quote API
+const quote = document.querySelector('#quote');
+const author = document.querySelector('#quote-author');
+
+async function getQuote() {
+  try {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    quote.textContent = data.content;
+    author.textContent = `- "${data.author}"`;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getQuote();
--- script.js
+++ script.js
@@ -1,90 +1,90 @@
 //? show and hide navbar
-const openSide = document.querySelector(".open-side");
-openSide.addEventListener("click", (e) => {
-  const navbar = document.querySelector(".navbar-content");
-  const arrow = document.querySelector(".arrow");
+const openSide = document.querySelector('.open-side');
+openSide.addEventListener('click', (e) => {
+  const navbar = document.querySelector('.navbar-content');
+  const arrow = document.querySelector('.arrow');
 
-  if (navbar.classList.contains("active")) {
-    navbar.parentElement.classList.add("active-width");
+  if (navbar.classList.contains('active')) {
+    navbar.parentElement.classList.add('active-width');
     setTimeout(() => {
-      navbar.classList.remove("active");
-      arrow.classList.add("rotate");
-      navbar.classList.add("mobile-width");
+      navbar.classList.remove('active');
+      arrow.classList.add('rotate');
+      navbar.classList.add('mobile-width');
     }, 350);
   } else {
-    navbar.parentElement.classList.remove("active-width");
+    navbar.parentElement.classList.remove('active-width');
     setTimeout(() => {
-      navbar.classList.add("active");
-      arrow.classList.remove("rotate");
-      navbar.classList.remove("mobile-width");
+      navbar.classList.add('active');
+      arrow.classList.remove('rotate');
+      navbar.classList.remove('mobile-width');
     }, 200);
   }
 
   //? hide if user click outside
-  document.addEventListener("click", function (e) {
-    const sidebarContainer = document.querySelector(".nav");
+  document.addEventListener('click', function (e) {
+    const sidebarContainer = document.querySelector('.nav');
     if (!sidebarContainer.contains(e.target)) {
-      navbar.parentElement.classList.remove("active-width");
+      navbar.parentElement.classList.remove('active-width');
       setTimeout(() => {
-        navbar.classList.add("active");
-        arrow.classList.remove("rotate");
-        navbar.classList.remove("mobile-width");
+        navbar.classList.add('active');
+        arrow.classList.remove('rotate');
+        navbar.classList.remove('mobile-width');
       }, 200);
     }
   });
 });
 
 // ! show and hide create task
-const addTaskBtn = document.querySelector("#add-task");
-addTaskBtn.addEventListener("click", (e) => {
-  addTaskBtn.classList.toggle("click");
+const addTaskBtn = document.querySelector('#add-task');
+addTaskBtn.addEventListener('click', (e) => {
+  addTaskBtn.classList.toggle('click');
 
-  const datePick = document.getElementById("date");
-  const timePick = document.getElementById("time");
+  const datePick = document.getElementById('date');
+  const timePick = document.getElementById('time');
 
   // ? default date on create
-  datePick.addEventListener("click", () => {
+  datePick.addEventListener('click', () => {
     datePick.value = dateNow();
   });
   // ? set date on click time
-  timePick.addEventListener("click", () => {
+  timePick.addEventListener('click', () => {
     datePick.value = dateNow();
   });
 
   // * form animation
-  const createTask = document.querySelector(".create-task");
-  if (createTask.classList.contains("onBlured")) {
-    createTask.classList.add("formOff");
+  const createTask = document.querySelector('.create-task');
+  if (createTask.classList.contains('onBlured')) {
+    createTask.classList.add('formOff');
     setTimeout(() => {
-      createTask.classList.remove("onBlured");
+      createTask.classList.remove('onBlured');
     }, 490);
   } else {
-    createTask.classList.remove("formOff");
-    createTask.classList.add("onBlured");
+    createTask.classList.remove('formOff');
+    createTask.classList.add('onBlured');
   }
 
   // * heading move up & down animation
-  const heading = document.querySelector(".heading-create-task");
-  if (heading.classList.contains("moveUp")) {
-    heading.classList.add("moveDown");
-    heading.classList.remove("moveUp");
+  const heading = document.querySelector('.heading-create-task');
+  if (heading.classList.contains('moveUp')) {
+    heading.classList.add('moveDown');
+    heading.classList.remove('moveUp');
   } else {
-    heading.classList.remove("moveDown");
-    heading.classList.add("moveUp");
+    heading.classList.remove('moveDown');
+    heading.classList.add('moveUp');
   }
 
   // ? close to click outside form box
-  document.addEventListener("click", function (e) {
-    const firstCol = document.querySelector(".first-col-head");
+  document.addEventListener('click', function (e) {
+    const firstCol = document.querySelector('.first-col-head');
     if (!firstCol.contains(e.target)) {
-      createTask.classList.add("formOff");
+      createTask.classList.add('formOff');
       setTimeout(() => {
-        createTask.classList.remove("onBlured");
+        createTask.classList.remove('onBlured');
       }, 490);
 
-      heading.classList.add("moveDown");
-      heading.classList.remove("moveUp");
-      addTaskBtn.classList.remove("click");
+      heading.classList.add('moveDown');
+      heading.classList.remove('moveUp');
+      addTaskBtn.classList.remove('click');
     }
   });
 });
@@ -93,37 +93,37 @@ function dateNow() {
   let today = new Date();
   let defaultDate =
     today.getFullYear() +
-    "-" +
-    ("0" + (today.getMonth() + 1)).slice(-2) +
-    "-" +
-    ("0" + today.getDate()).slice(-2);
+    '-' +
+    ('0' + (today.getMonth() + 1)).slice(-2) +
+    '-' +
+    ('0' + today.getDate()).slice(-2);
 
   return defaultDate;
 }
 
 // ! changing task section in mobile screen
-const track = document.querySelector(".all-todo");
-const taskSections = document.querySelectorAll("[data-listItem]");
+const track = document.querySelector('.all-todo');
+const taskSections = document.querySelectorAll('[data-listItem]');
 
 // ? click on heading (dots)
 taskSections.forEach((section, index) => {
-  section.addEventListener("click", (e) => {
+  section.addEventListener('click', (e) => {
     const containerWidth = track.getBoundingClientRect().width;
 
     if (e.target === taskSections[index]) {
       track.scrollTo({
         left: Math.round(containerWidth) * index,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     }
   });
 });
 // ? scrolling lists
-track.addEventListener("scroll", (e) => {
-  const slides = document.querySelectorAll("[data-slide]");
+track.addEventListener('scroll', (e) => {
+  const slides = document.querySelectorAll('[data-slide]');
 
   taskSections.forEach((section) => {
-    section.classList.remove("visible");
+    section.classList.remove('visible');
   });
 
   const currentX = track.scrollLeft;
@@ -132,5 +132,5 @@ track.addEventListener("scroll", (e) => {
   //? determine which index is active
   const nthchild = Math.round(currentX / scrolllength);
 
-  taskSections[nthchild].classList.add("visible");
+  taskSections[nthchild].classList.add('visible');
 });
